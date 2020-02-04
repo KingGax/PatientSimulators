f26519bce9356f403cf53610663242a82fb0e528
ralph <ralphrobertsuk@gmail.com>
2020-02-04 00:27:32 +0000

    Takes the headers out of the csv and allows you to add them to a list view ---- needs error checking in multiple places

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
@@ -11,6 +17,7 @@ import javafx.scene.layout.StackPane;
 import java.awt.*;
 import javafx.event.ActionEvent;
 
+import javax.swing.*;
 import java.io.*;
 import java.nio.Buffer;
 import java.util.logging.Logger;
@@ -18,40 +25,61 @@ import java.util.logging.Logger;
 public class Main extends Application {
     private Desktop desktop = Desktop.getDesktop();
     BufferedReader reader;
+    ComboBox<String> headerPicker;
     @Override
     public void start(Stage stage) {
         reader = null;
-        Label label1 = new Label("Welcome to Patient Simulators");
+        Label title = new Label("Welcome to Patient Simulators");
         Button fileSelectorButton = new Button("Select File");
+        Button simulationButton = new Button("Run Simulation");
+        simulationButton.setOnAction(e -> SetDashboadScene());
         FileChooser fileChooser = new FileChooser();
-        fileSelectorButton.setOnAction(e -> {
-            File file = fileChooser.showOpenDialog(stage);
-            openFile(file);
-            try {
-                System.out.println(reader.readLine());
-            } catch (IOException ex) {
-                System.out.println("IOException in printing file");
-            }
-        });
-        Scene welcome = new Scene(new StackPane(label1, fileSelectorButton), 640, 480);
+        fileSelectorButton.setOnAction(e -> openFile(fileChooser,stage));
+        ListView<String>selectedHeaders = new ListView<String>();
+        headerPicker =  new ComboBox<String>();
+        headerPicker.setPromptText("Choose a file to select headers");
+        Button addHeader = new Button("Add Header");
+        addHeader.setOnAction(e -> selectedHeaders.getItems().addAll(headerPicker.getValue()));
+        HBox chooseHeadersBox = new HBox(20);
+        chooseHeadersBox.setAlignment(Pos.CENTER);
+        VBox centreBox = new VBox(30);
+        chooseHeadersBox.getChildren().addAll(headerPicker,addHeader,selectedHeaders);
+        centreBox.getChildren().addAll(title,fileSelectorButton,chooseHeadersBox,simulationButton);
+        centreBox.setAlignment(Pos.CENTER);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(centreBox);
+        Scene welcome = new Scene(borderPane, 640, 480);
         stage.setScene(welcome);
         stage.setTitle("Patient Simulators");
         fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(stage);
         stage.show();
+
+    }
+    void SetDashboadScene()
+    {
 
     }
 
 
-    public void openFile(File f){
+    public void openFile(FileChooser fileChooser,Stage stage){
+        File file = fileChooser.showOpenDialog(stage);
         try {
-            reader = new BufferedReader(new FileReader(f));
+            reader = new BufferedReader(new FileReader(file));
         } catch (IOException ex){
             reader = new BufferedReader(null);
             System.out.println("IOException in opening file");
         }
-    }
+            try {
+                String commaSep = reader.readLine();
+                fillComboBoxFromReader(commaSep);
+            } catch (IOException ex) {
+                System.out.println("IOException in printing file");
+            }
 
+    }
+    public void fillComboBoxFromReader(String commaSepItems){
+        headerPicker.getItems().addAll(commaSepItems.split(","));
+    }
 
     public static void main(String[] args) {
         launch();
