a7c340c9e979a54a7f52a3af1756af21c0a279eb
Noobphail <rob.russmann@googlemail.com>
2020-02-06 00:45:15 +0000

    Cleaned code a bit, added array to store file data. Now retrieves correct data from file and places it in dataArray

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,19 +13,17 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import java.awt.*;
-import javafx.event.ActionEvent;
 
-import javax.swing.*;
 import java.io.*;
-import java.nio.Buffer;
-import java.util.logging.Logger;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main extends Application {
-    private Desktop desktop = Desktop.getDesktop();
-    BufferedReader reader;
-    ComboBox<String> headerPicker;
+    private BufferedReader reader;
+    private int rowCount;
+    private float[][] dataArray; //Ignore this warning, dataArray to be used for gauges
+    private ComboBox<String> headerPicker;
     @Override
     public void start(Stage stage) {
         reader = null;
@@ -35,8 +32,9 @@ public class Main extends Application {
         Button simulationButton = new Button("Run Simulation");
         FileChooser fileChooser = new FileChooser();
         fileSelectorButton.setOnAction(e -> openFile(fileChooser,stage));
-        ListView<String>selectedHeaders = new ListView<String>();
-        selectedHeaders.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        ListView<String>selectedHeaders = new ListView<>();
+        selectedHeaders.setOnMouseClicked(e -> handleMouse(e,selectedHeaders));
+        /*new EventHandler<MouseEvent>() {
 
                                        @Override
                                        public void handle(MouseEvent click) {
@@ -46,8 +44,8 @@ public class Main extends Application {
                                                selectedHeaders.getItems().remove(selectedHeaders.getSelectionModel().getSelectedItem());
                                            }
                                        }
-                                   });
-        headerPicker =  new ComboBox<String>();
+                                   });*/
+        headerPicker =  new ComboBox<>();
         headerPicker.setPromptText("Choose a file to select headers");
         Button addHeader = new Button("Add Header");
         addHeader.setOnAction(e -> tryAddItem(headerPicker.getValue(),selectedHeaders));
@@ -67,28 +65,96 @@ public class Main extends Application {
         stage.show();
 
     }
-    void tryAddItem(String item, ListView<String> lv)
+
+    private void handleMouse(MouseEvent click, ListView<String> selectedHeaders){
+        if (click.getClickCount() == 2) {
+            //Use ListView's getSelected Item
+            selectedHeaders.getItems().remove(selectedHeaders.getSelectionModel().getSelectedItem());
+        }
+    }
+    private void tryAddItem(String item, ListView<String> lv)
     {
         if (!lv.getItems().contains(item)&&(item!=null)){
             lv.getItems().add(item);
         }
     }
-    Scene getDashboardScene(ListView<String>selectedItems)
+    private Scene getDashboardScene(ListView<String>selectedItems)
     {
-
+        fillDataArray(selectedItems);
         BorderPane bp = new BorderPane();
         Scene simulation = new Scene(bp, 640, 480);
         return simulation;
     }
 
+    private void fillDataArray(ListView<String>selectedItems){
+        dataArray = new float[rowCount][selectedItems.getItems().size()];
+        System.out.println(selectedItems.getItems().get(0));
+        int[][] offSetArray = new int [selectedItems.getItems().size()][2];
+        for (int i=0; i < dataArray.length; i++){
+            String[] tempData = new String[1];
+            try{
+                tempData = reader.readLine().split(", ");
+            }
+            catch (IOException ex){
+                System.out.println("IOException in reading from file.");
+            }
+            if (i == 0){
+                for (int j = 0; j < dataArray[i].length; j++){
+                    offSetArray[j][0] = j;
+                    offSetArray[j][1] = Arrays.asList(tempData).indexOf(selectedItems.getItems().get(j));
+                }
+            } else {
+                for (int j = 0; j < dataArray[i].length; j++) {
+                    if (offSetArray[j][1] == 0 || offSetArray[j][1] == 1) { //Time
+                        dataArray[i][j] = (float) Math.round(timeToFloat(tempData[offSetArray[j][1]]) * 10000f) / 10000f;
+                    } else if (offSetArray[j][1] == 2) { //Date
+                        dataArray[i][j] = dateTimeToFloat(tempData[offSetArray[j][1]]);
+                    } else { //Regular float
+                        dataArray[i][j] = (float) Math.round(Float.parseFloat(tempData[offSetArray[j][1]])*10000f)/10000f;
+                    }
+                }
+            }
+        }
+    }
 
+    private float dateTimeToFloat(String date){
+        float outVal;
+        String pattern = "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(date);
+        if (!m.find()){
+            return 0;
+        }
+        else{
+            int years = Integer.parseInt(date.substring(0, 4));
+            int months = Integer.parseInt(date.substring(5, 7));
+            int days = Integer.parseInt(date.substring(8, 10));
+            outVal = timeToFloat((24*(days+28*months+365*years))+":00:00")+timeToFloat(date.substring(11,19));
+            return outVal;
+        }
+    }
 
-    public void openFile(FileChooser fileChooser,Stage stage){
+    private float timeToFloat(String time){
+        float outVal;
+        String pattern = "\\d+\\d:\\d\\d:\\d\\d";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(time);
+        if (!m.find()){ //r̝̮̥͔͎̱̜eg̭̺̰̪e̮̝͕̗̙̗ͅx̨
+            return 0;
+        } else {
+            int hours = Integer.parseInt(time.substring(0,2));
+            int minutes = Integer.parseInt(time.substring(3,5));
+            int seconds = Integer.parseInt(time.substring(6,8));
+            outVal = seconds + 60*(minutes+60*hours);
+            return outVal;
+        }
+    }
+
+    private void openFile(FileChooser fileChooser,Stage stage){
         File file = fileChooser.showOpenDialog(stage);
         try {
             reader = new BufferedReader(new FileReader(file));
         } catch (IOException ex){
-            reader = new BufferedReader(null);
             System.out.println("IOException in opening file");
         }
             try {
@@ -99,11 +165,21 @@ public class Main extends Application {
             } catch (IOException ex) {
                 System.out.println("IOException in printing file");
             }
+            try{
+                BufferedReader rowReader = new BufferedReader(new FileReader(file));
+                rowCount = 0;
+                while (rowReader.readLine() != null) {
+                    rowCount++;
+                }
+                rowReader.close();
+            } catch (IOException ex) {
+                System.out.println("IOException in counting rows");
+            }
 
     }
-    public void fillComboBoxFromReader(String commaSepItems){
+    private void fillComboBoxFromReader(String commaSepItems){
         headerPicker.getItems().clear();
-        headerPicker.getItems().addAll(commaSepItems.split(","));
+        headerPicker.getItems().addAll(commaSepItems.split(", "));
     }
 
     public static void main(String[] args) {
