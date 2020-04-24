63bb5733298bda8ce8d65c40df77c55eac733477
Noobphail <rob.russmann@gmail.com>
2020-04-24 20:52:29 +0100

    Trying to get mvn package to actually make a functioning jar :(

---
+++ src/main/java/sample/SuperMain.java
@@ -0,0 +1,7 @@
+package sample;
+//This needs to exist as main class cannot extend anything (marked this as main class in pom.xml so it *should* run)
+public class SuperMain {
+    public static void main(String[] args){
+        Main.main(args);
+    }
+}
